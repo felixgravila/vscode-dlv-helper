20adb923cc2c0738b1fc1f73196aceb52f0f4d8e
Felix Gravila <felixgravila@gmail.com>
2018-11-14 07:43:40 +0100

    made extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,11 +15,46 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+
+    let disposable = vscode.commands.registerCommand('dlvhelper.runselect', () => {
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return; // No open text editor
+        }
+        let document = editor.document.fileName;
+        let filename = path.basename(document);
+        let extension = path.extname(document);
+        if(extension !== ".dl") {
+            return;
+        }
+
+        // vscode.window.showInformationMessage(filename);
+
+        let csl = vscode.window.activeTerminal || vscode.window.createTerminal("dlv");
+
+        let selection = editor.selection;
+        let text = editor.document.getText(selection);
+
+        let singlewordregex = /^([a-zA-Z0-9]+)$/g;
+        let singleword = (text.match(singlewordregex) || []).map(e => e.replace(singlewordregex, '$1'));
+        if(singleword.length === 1 && singleword[0] === text) {
+            //only one word selected
+            csl.sendText('dlv '+document+' -filter="'+singleword[0]+'"');
+        } else {
+
+            let rgx = / *([a-z][a-zA-Z0-9]*) *\([A-Z][a-zA-Z0-9,]*\) *:-/g;
+            let matches = (text.match(rgx) || []).map(e => e.replace(rgx, '$1'));
+
+            let args = "";
+            for (let fltr of matches) {
+                args = args + ' -filter="' + fltr + '"';
+            }
+
+            csl.sendText("dlv "+document+args);
+        }
+
+
     });
 
     context.subscriptions.push(disposable);
